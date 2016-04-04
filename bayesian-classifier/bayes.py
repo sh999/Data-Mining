2cbf67cb67a6a7a6be2f4ab17b6ddc00f14f276f
Satrio <sh999gh@gmail.com>
2016-04-04 06:31:47 -0400

    Sample data structure

--- bayesian-classifier/bayes.py
+++ bayesian-classifier/bayes.py
@@ -1,3 +1,6 @@
+'''
+Bayesian classifier for car data
+'''
 classifications = ["unacc","acc","good","vgood"]
 buying = ["vghigh","high","med","low"]
 maint = ["vghigh","high","med","low"]
@@ -6,5 +9,38 @@ persons = ["2","4","more"]
 lug_boot = ["small","med","big"]
 safety = ["low","med","high"]
 inputfile = open("small.data")
+attributes = [classifications, buying, maint, doors, persons, lug_boot, safety]
+
 small = "med,low,3,more,big,low,unacc"
+'''
+pseudocode:
+For each line in file:
+	Look at the last attribute for classification; remember this
+	For each attribute:
+
+Sample data structure:
+unacc = 10
+	   buying = 
+	   		   vhigh = 3
+	   		   high  = 4
+	   		   med   = 1
+	   		   low   = 2
+	   maint = 
+	   		   vhigh ...
+	   doors = 
+	   .
+	   .
+	   .
+	   safety = 
+	           low = 9
+	           med = 1
+	           high = 0
+acc = 32
+		.
+		.
+		.
+		.
+good = 
+
+'''
 
