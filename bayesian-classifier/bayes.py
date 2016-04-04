362e8e9c1b19b857c2dbfd0d283298b443c5fb6f
Satrio <sh999gh@gmail.com>
2016-04-04 10:19:08 -0400

    Initial table with 0s and attempts at automation included

--- bayesian-classifier/bayes.py
+++ bayesian-classifier/bayes.py
@@ -1,15 +1,7 @@
 '''
 Bayesian classifier for car data
 '''
-'''
-classifications = ["unacc","acc","good","vgood"]
-buying = ["vghigh","high","med","low"]
-maint = ["vghigh","high","med","low"]
-doors = ["2","3","4","5more"]
-persons = ["2","4","more"]
-lug_boot = ["small","med","big"]
-safety = ["low","med","high"]
-'''
+
 
 
 '''
@@ -42,23 +34,104 @@ acc = 32
 		.
 good = 
 
-{unacc:{count:10,buying:{vhigh:3,high:4...},maint:{vhigh...},...,acc:{count:32,buying:{......}}}
+{"unacc":{count:10,buying:{vhigh:3,high:4...},maint:{vhigh...},...,"acc":{count:32,buying:{......}}}
 
 '''
+
+'''
+classifications = ["unacc","acc","good","vgood"]
+buying = ["vghigh","high","med","low"]
+maint = ["vghigh","high","med","low"]
+doors = ["2","3","4","5more"]
+persons = ["2","4","more"]
+lug_boot = ["small","med","big"]
+safety = ["low","med","high"]
+'''
+'''
+buying = ["vghigh","high","med","low"]
+maint = ["vghigh","high","med","low"]
+doors = ["2","3","4","5more"]
+persons = ["2","4","more"]
+lug_boot = ["small","med","big"]
+safety = ["low","med","high"]
+'''
 classifications = {"unacc","acc","good","vgood"}
-buying = {"vghigh":0,"high":0,"med":0,"low":0}
-maint = {"vghigh":0,"high":0,"med":0,"low":0}
-doors = {"2":0,"3":0,"4":0,"5more":0}
-persons = {"2":0,"4":0,"more":0}
-lug_boot = {"small":0,"med":0,"big":0}
-safety = {"low":0,"med":0,"high":0}
-attributes = [classifications, buying, maint, doors, persons, lug_boot, safety]
 
-table = {x:{'count':0} for x in classifications} 
-for car_class, v in table.iteritems():
-	for attr, val in buying.iteritems():
-		table[car_class][attr] = 0
+'''
+buying = {"label":"buying","vghigh":0,"high":0,"med":0,"low":0}
+maint = {"label":"maint","vghigh":0,"high":0,"med":0,"low":0}
+doors = {"label":"doors","2":0,"3":0,"4":0,"5more":0}
+persons = {"label":"persons","2":0,"4":0,"more":0}
+lug_boot = {"label":"lug_boot","small":0,"med":0,"big":0}
+safety = {"label":"safety","low":0,"med":0,"high":0}
+'''
 
+table = {"unacc":{
+				"count":0,
+				"buying":{"vghigh":0,"high":0,"med":0,"low":0},
+				"maint":{"vghigh":0,"high":0,"med":0,"low":0},
+				"doors":{"2":0,"3":0,"4":0,"5more":0},
+				"persons":{"2":0,"4":0,"more":0},
+				"lug_boot":{"small":0,"med":0,"big":0},
+				"safety":{"low":0,"med":0,"high":0}
+				}
+		,
+		"acc":{
+				"count":0,
+				"buying":{"vghigh":0,"high":0,"med":0,"low":0},
+				"maint":{"vghigh":0,"high":0,"med":0,"low":0},
+				"doors":{"2":0,"3":0,"4":0,"5more":0},
+				"persons":{"2":0,"4":0,"more":0},
+				"lug_boot":{"small":0,"med":0,"big":0},
+				"safety":{"low":0,"med":0,"high":0}
+				}
+		,
+		"good":{
+				"count":0,
+				"buying":{"vghigh":0,"high":0,"med":0,"low":0},
+				"maint":{"vghigh":0,"high":0,"med":0,"low":0},
+				"doors":{"2":0,"3":0,"4":0,"5more":0},
+				"persons":{"2":0,"4":0,"more":0},
+				"lug_boot":{"small":0,"med":0,"big":0},
+				"safety":{"low":0,"med":0,"high":0}
+				}
+		,
+		"vgood":{
+				"count":0,
+				"buying":{"vghigh":0,"high":0,"med":0,"low":0},
+				"maint":{"vghigh":0,"high":0,"med":0,"low":0},
+				"doors":{"2":0,"3":0,"4":0,"5more":0},
+				"persons":{"2":0,"4":0,"more":0},
+				"lug_boot":{"small":0,"med":0,"big":0},
+				"safety":{"low":0,"med":0,"high":0}
+				}
+		}
+print table
+'''
+buying_vals = {"vghigh":0,"high":0,"med":0,"low":0}
+maint_vals = {"vghigh":0,"high":0,"med":0,"low":0}
+doors_vals = {"2":0,"3":0,"4":0,"5more":0}
+persons_vals = {"2":0,"4":0,"more":0}
+lug_boot_vals = {"small":0,"med":0,"big":0}
+safety_vals = {"low":0,"med":0,"high":0}
+
+# attributes = {"buying":0, "maint":0,"doors":0,"persons":0,"lug_boot":0,"safety":0}
+attributes = ["buying", "maint", "doors", "persons", "lug_boot", "safety"]
+# a = {i:0 for i in attributes}
+# print "a:",a
+buying = {"buying":{k:v for k, v in buying_vals.iteritems()}} 
+maint = {"maint":{k:v for k, v in maint_vals.iteritems()}} 
+doors = {"doors":{k:v for k, v in doors_vals.iteritems()}} 
+persons = {"persons":{k:v for k, v in persons_vals.iteritems()}} 
+lug_boot = {"lug_boot":{k:v for k, v in lug_boot_vals.iteritems()}} 
+safety = {"safety":{k:v for k, v in safety_vals.iteritems()}} 
+
+print "buying:",buying
+# table = {x:{'count':0} for x in classifications} 
+# for car_class, v in table.iteritems():
+	# print car_class
+table = {"count":0, {""}}
+'''
 print "table:\n",table
 
 # inputfile = open("small.data")
