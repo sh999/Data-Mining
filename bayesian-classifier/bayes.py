5f199238a260e37bc7f7ba4183ba03e1006ec344
Satrio <sh999gh@gmail.com>
2016-04-03 21:37:34 -0400

    Add bayesian classifier

---
+++ bayesian-classifier/bayes.py
@@ -0,0 +1,10 @@
+classifications = ["unacc","acc","good","vgood"]
+buying = ["vghigh","high","med","low"]
+maint = ["vghigh","high","med","low"]
+doors = ["2","3","4","5more"]
+persons = ["2","4","more"]
+lug_boot = ["small","med","big"]
+safety = ["low","med","high"]
+inputfile = open("small.data")
+small = "med,low,3,more,big,low,unacc"
+
